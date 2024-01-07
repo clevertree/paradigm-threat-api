9b089dfc31afebff693db5d438dcc0a4d5a2217d
Ari Asulin <ari.asulin@gmail.com>
2024-01-07 11:49:36 -0500

    allow cors

--- next.config.js
+++ next.config.js
@@ -61,24 +61,24 @@ const nextConfig = {
     })
     //
     return config
+  },
+  async headers () {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ]
   }
-  // async headers () {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: '/api/:path*',
-  //       headers: [
-  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-  //         { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-  //         { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // }
 }
 
 // Merge MDX config with Next.js config
